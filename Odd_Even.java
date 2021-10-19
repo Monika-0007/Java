846219f7a1d2f95d29427ec72e908f3ea9d08f88
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-10-19 15:39:19 +0530

    Update and rename Print_even_numbers to Odd_Even.java

---
+++ Odd_Even.java
@@ -0,0 +1,42 @@
+public class oddeven{
+    public static void main(String args[]) {
+      int i;
+      for(i=1;i<=100;i++)
+    {
+        if(i%2==0)
+            {
+            System.out.println("Even Numbers:");
+            System.out.println(i);
+            }
+          else
+              {
+              System.out.println("Odd Numbers");
+              System.out.println(i);
+              }
+          
+    }
+}
+}
+public class oddeven{
+    public static void main(String args[]) {
+      int i;
+     System.out.println("Even Numbers:");
+      for(i=1;i<=100;i++)
+    { 
+        if(i%2==0)
+            {
+            System.out.println(i);
+            }
+    
+    }
+    System.out.println("Odd Numbers:");
+      for(i=1;i<=100;i++)
+    { 
+        if(i%2!=0)
+            {
+            System.out.println(i);
+            }
+    
+    }
+}
+}
