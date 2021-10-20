bbe3067cf53686b95b5246b70f0b342b34bfc59a
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-10-20 10:07:23 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,22 @@
+public class variable {
+    public static void main(String args[]) {
+        byte age=22;
+        int pincode=560036;
+        long phone_number=8123701748L;
+        short salary=30000;
+        char section='A';
+        boolean is_indian=true;
+        float percentage=89.83f;
+        double data=2881278.267842697d;
+        String name="Monika";
+        System.out.println("Name: "+name); 
+        System.out.println("Age: "+age); 
+        System.out.println("Pincode: "+pincode); 
+        System.out.println("Phone Number: "+phone_number); 
+        System.out.println("Salary: "+salary); 
+        System.out.println("Section: "+section); 
+        System.out.println("Indian: "+is_indian); 
+        System.out.println("Percentage: "+percentage); 
+        System.out.println("Data: "+data); 
+    }
+}
