2f7319112af2c02ef94b218d901668ac4bfad38e
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-10-20 11:37:10 +0530

    Create checkprime.java

---
+++ checkprime.java
@@ -0,0 +1,16 @@
+public class checkprime {
+    public static void main(String args[]) {
+       int num=100,count=0;
+       for(int i=1;i<=num;i++){
+       if(num%i==0){
+           count++;
+       }
+      }
+       if(count==2){
+      System.out.println(num+" is a prime number");
+    }
+     else{
+      System.out.println(num+" is not a prime number");
+    }
+    }
+}
