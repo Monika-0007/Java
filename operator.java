8e262f07c1dd64ff891e8dd325d8f7b084ed5abc
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-10-20 10:36:59 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,22 @@
+public class operator {
+    public static void main(String args[]) {
+        int a=500,b=1000,c=1500;
+        //AND Operator
+        if(a>b && a>c){
+            System.out.println(a);
+        }
+        else if(b>a && b>c) {
+            System.out.println(b);
+        }
+        else {
+            System.out.println(c);
+        }
+        //OR Operator
+        if(b>0 || b<0){
+            System.out.println("Not Zero");
+        }
+        else {
+            System.out.println("Zero");
+        }
+    }
+}
