4c418c6bdf6e09bfa2a799ffa8f1589b3b1489b9
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-10-20 11:39:44 +0530

    Update numberoffactors.java

--- numberoffactors.java
+++ numberoffactors.java
@@ -1,9 +1,10 @@
 public class no_of_factors {
     public static void main(String args[]) {
        int num=100,count=0;
-       for(int i=1;i<=num;i++)
+       for(int i=1;i<=num;i++){
        if(num%i==0){
            count++;
+       }
        }
       System.out.println(count);
     }
