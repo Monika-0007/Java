1bbc024b4e8dc052ea3b0fedd923d2df4bd3fa36
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-10-20 10:21:42 +0530

    Create greaternumber.java

---
+++ greaternumber.java
@@ -0,0 +1,11 @@
+public class GreaterNumber {
+    public static void main(String args[]) {
+        int num1=500,num2=1000;
+        if(num1 > num2){
+            System.out.println(num1);
+        }
+        else{
+            System.out.println(num2);
+        }
+    }
+}
