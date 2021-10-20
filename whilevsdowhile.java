a6dd057f7582144c0d20fc92a87d06bcdcabda84
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-10-20 11:23:41 +0530

    Create whilevsdowhile.java

---
+++ whilevsdowhile.java
@@ -0,0 +1,13 @@
+public class whilevsdowhile {
+    public static void main(String args[]) {
+       int num1=0;
+       while(num1>=1){
+           System.out.println(num1);
+           num1--;
+       }
+       do{
+           System.out.println(num1);
+           num1--;
+       }while(num1>=1);
+    }
+}
