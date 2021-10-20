705a2505d4e8528b88bd7868f3513ee0e7086754
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-10-20 10:15:08 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    public static void main(String args[]) {
+        int num1=500,num2=1000;
+        if(num1 == num2){
+            System.out.println("Equal");
+        }
+        else{
+            System.out.println("Not Equal");
+        }
+    }
+}
