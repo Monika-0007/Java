56b9d531ba82758e77a61d658290e4bc1cfcdbd4
Monika-0007 <68844195+Monika-0007@users.noreply.github.com>
2021-10-20 10:27:05 +0530

    Create elseif.java

---
+++ elseif.java
@@ -0,0 +1,14 @@
+public class elseif {
+    public static void main(String args[]) {
+        int num=5000;
+        if(num > 0){
+            System.out.println("Positive Number");
+        }
+        else if(num < 0) {
+            System.out.println("Negative Number");
+        }
+        else {
+            System.out.println("Zero");
+        }
+    }
+}
